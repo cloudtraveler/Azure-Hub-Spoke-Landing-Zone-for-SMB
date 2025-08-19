c68cec3bdc9b94e3d3ac300f41da6ef433e7559e
Seokwoo Sean Kim <49170254+cloudtraveler@users.noreply.github.com>
2025-08-19 12:17:10 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,4 +12,6 @@ provider "azurerm" {
   features {}
   # Azure 인증은 Azure CLI (az login) or ARM_* environment variables.
   # 필요하면 ID값 환경 설정 set ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, and ARM_SUBSCRIPTION_ID.
+  # subscription_id = " "
+  # tenant_id       = " "
 }
